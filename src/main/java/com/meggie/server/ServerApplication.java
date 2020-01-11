e0d3f7cb7325493bdbaaf21fcca34731ebcfd04a
t4ub3 <jn.jonasneumann@googlemail.com>
2020-01-11 14:19:14 +0100

    init

---
+++ src/main/java/com/meggie/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.meggie.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
